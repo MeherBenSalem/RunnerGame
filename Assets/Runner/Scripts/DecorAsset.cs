3aa638cd53154a58e1726453fffc0437773a62f4
M3herBenSalem <mahourabensalem@gmail.com>
2023-02-25 00:26:40 +0100

    Deco_Commit

---
+++ Assets/Runner/Scripts/DecorAsset.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using HyperCasual.Core;
+using UnityEngine;
+namespace HyperCasual.Runner
+{
+    public class DecorAsset : Spawnable
+    {
+    }
+}
